f7b51341ce27f8ed9a4a8b1bac5e8658ddb2b701
Kevin Babcock and Malgorzata Haniszewska <malgorzata@>
2017-11-01 11:13:25 -0700

    place scramled letters in new array

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,12 @@ $(document).ready(function() {
     event.preventDefault();
     var inputString = $("#inputString").val();
 
-
-
     var clearString = inputString.replace(/[^a-zA-Z]/g, "");
     var clearStringLength = clearString.length;
     var numberOfRows = Math.ceil(Math.sqrt(clearStringLength));
     var numberOfColumns = Math.floor(Math.sqrt(clearStringLength));
     var arrayOfRows = [];
+    var outputArray = [];
 
     for (index = 0; index < numberOfRows; index ++) {
       var newArray = [];
@@ -19,17 +18,22 @@ $(document).ready(function() {
 
     var characters = clearString.split("");
 
-    //console.log(numberOfColumns);
+
     for (var index = 0; index < numberOfRows; index++) {
       for (var j = index * numberOfColumns; j < numberOfColumns * (index + 1); j++) {
         arrayOfRows[index].push(characters[j]);
-
       }
-
     }
-    arrayOfRows.forEach(function(row) {
-      console.log(row);
-    })
+
+    for (var index = 0; index < numberOfColumns; index++) {
+      for (var j = 0; j < arrayOfRows.length; j++) {
+        var letter = arrayOfRows[j];
+        outputArray.push(letter[index]);
+        // outputArray.push(arrayOfRows[index]);
+      }
+    }
+    console.log(outputArray);
+
   })
 
 })
